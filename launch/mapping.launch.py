571c6bb868d9d58bcd89e16f2865258720dffd34
Augusto-Viniciuss <augustoviniciud@gmail.com>
2025-10-29 13:38:43 -0300

    fix launch

--- launch/mapping.launch.py
+++ launch/mapping.launch.py
@@ -14,7 +14,7 @@ def generate_launch_description():
     package_path = get_package_share_directory('fast_lio')
     default_config_path = os.path.join(package_path, 'config')
 
-    declare_namespace = DeclareLaunchArgument(
+    declared_namespace = DeclareLaunchArgument(
         'namespace',
         default_value=EnvironmentVariable('UAV_NAME'),
         description='Namespace do nó.'
@@ -24,16 +24,32 @@ def generate_launch_description():
 
     use_sim_time = LaunchConfiguration('use_sim_time')
     config_file_path = LaunchConfiguration('config_file_path')
+    topic_pcl = LaunchConfiguration('topic_pcl')
+    topic_imu = LaunchConfiguration('topic_imu')
 
-    declare_use_sim_time = DeclareLaunchArgument(
+    declared_use_sim_time = DeclareLaunchArgument(
         'use_sim_time', default_value='false',
         description='Use simulation (Gazebo) clock if true'
     )
 
-    declare_config_file_path = DeclareLaunchArgument(
+    declared_config_file_path = DeclareLaunchArgument(
         'config_file_path', default_value='',
         description='Yaml config file path'
     )
+
+    declared_topic_imu = 
+        DeclareLaunchArgument(
+            'topic_imu',
+            default_value=['/', os.getenv('UAV_NAME', "uav1"), '/livox/imu'],
+            description='Name of the IMU topic.'
+    )
+
+    declared_topic_pcl = DeclareLaunchArgument(
+            'topic_pcl',
+            default_value=['/', os.getenv('UAV_NAME', "uav1"), '/livox/lidar'],
+            description='Name of the pcl topic.'
+    )
+
 
     fast_lio_node = Node(
         package='fast_lio',
@@ -41,8 +57,8 @@ def generate_launch_description():
         namespace=namespace,
         parameters=[config_file_path, {'use_sim_time': use_sim_time}],
         remappings=[
-            ('lidar_in', 'livox/points'),
-            ('imu_in', 'livox/imu'),
+            ('lidar_in', topic_pcl),
+            ('imu_in', topic_imu),
             ('imu_out', 'fast_lio/imu/out'),
             ('cloud_registered_body_out', 'fast_lio/cloud_registered_body'),
             ('cloud_registered_out', 'fast_lio/cloud_registered'),
@@ -59,10 +75,13 @@ def generate_launch_description():
     ld = LaunchDescription()
     
     # 3. Adiciona a declaração do argumento ao LaunchDescription
-    ld.add_action(declare_namespace)
+    ld.add_action(declared_namespace)
     
-    ld.add_action(declare_use_sim_time)
-    ld.add_action(declare_config_file_path)
+    ld.add_action(declared_use_sim_time)
+    ld.add_action(declared_config_file_path)
+
+    ld.add_action(declared_topic_pcl)
+    ld.add_action(declared_topic_imu)
 
     ld.add_action(fast_lio_node)
 
